1768ec79ccde2992846b6f77f0478f8c84042584
SputniksDreams <51749588+SputniksDreams@users.noreply.github.com>
2022-03-25 18:00:30 -0500

    set up user api

--- controllers/thoughts-controller.js
+++ controllers/thoughts-controller.js
@@ -2,6 +2,16 @@ const { Thought, User } = require('../models');
 
 const thoughtController = {
   // add thought to User
+  getAllThoughts(req, res) {
+    Thought.find({})
+      .select('-__v')
+      .sort({ _id: -1 })
+      .then(dbUserData => res.json(dbUserData))
+      .catch(err => {
+        console.log(err);
+        res.sendStatus(400);
+      });
+  },
   addThought({ params, body }, res) {
     console.log(params);
     Thought.create(body)
@@ -22,25 +32,18 @@ const thoughtController = {
       })
       .catch(err => res.json(err));
   },
-
-  // add reaction to Thought
-  addReply({ params, body }, res) {
-    Thought.findOneAndUpdate(
-      { _id: params.thoughtId },
-      { $push: { reactions: body } },
-      { new: true, runValidators: true }
-    )
+  updateThought({ params, body }, res) {
+    Thought.findOneAndUpdate({ _id: params.id }, body, { new: true, runValidators: true })
       .then(dbUserData => {
         if (!dbUserData) {
-          res.status(404).json({ message: 'No User found with this id!' });
+          res.status(404).json({ message: 'No Thought found with this id!' });
           return;
         }
         res.json(dbUserData);
       })
       .catch(err => res.json(err));
   },
-
-  // remove Thought
+    // remove Thought
   removeThought({ params }, res) {
     Thought.findOneAndDelete({ _id: params.thoughtId })
       .then(deletedThought => {
@@ -62,7 +65,23 @@ const thoughtController = {
       })
       .catch(err => res.json(err));
   },
-  // remove reply
+  // add reaction to Thought
+  addReaction({ params, body }, res) {
+    Thought.findOneAndUpdate(
+      { _id: params.thoughtId },
+      { $push: { reactions: body } },
+      { new: true, runValidators: true }
+    )
+      .then(dbUserData => {
+        if (!dbUserData) {
+          res.status(404).json({ message: 'No User found with this id!' });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch(err => res.json(err));
+  },
+  // remove reaction
   removeReaction({ params }, res) {
     Thought.findOneAndUpdate(
       { _id: params.thoughtId },
---
+++ routes/api/user-routes.js
@@ -0,0 +1,30 @@
+const router = require('express').Router();
+const {
+  getAllUser,
+  getUserById,
+  createUser,
+  updateUser,
+  deleteUser,
+  addFriend,
+  removeFriend
+} = require('../../controllers/user-controller');
+
+// /api/pizzas
+router
+  .route('/')
+  .get(getAllUser)
+  .post(createUser);
+
+// /api/pizzas/:id
+router
+  .route('/:id')
+  .get(getUserById)
+  .put(updateUser)
+  .delete(deleteUser);
+
+router
+.route('/:id/friends/:friendId')
+  .put(addFriend)
+  .delete(removeFriend);
+module.exports = router;
+
